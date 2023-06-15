bcfbef2a32106462340eb56d4b91c10aa159edd4
Harshith <saiharshithkaranam@gmail.com>
2023-06-15 20:24:48 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-function App() {
+const NumberManagement = () => {
+  const [numbers, setNumbers] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const urls = [
+        'http://104.211.219.98/numbers/primes',
+        'http://104.211.219.98/numbers/fibo',
+        'http://104.211.219.98/numbers/odd'
+      ];
+
+      const responses = await Promise.all(
+        urls.map(url => axios.get(url))
+      );
+
+      const mergedNumbers = responses.reduce((acc, response) => {
+        if (response.status === 200) {
+          const { numbers } = response.data;
+          return [...acc, ...numbers];
+        }
+        return acc;
+      }, []);
+
+      const uniqueNumbers = [...new Set(mergedNumbers)];
+      setNumbers(uniqueNumbers.sort());
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Number Management</h1>
+      <ul>
+        {numbers.map(number => (
+          <li key={number}>{number}</li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
-export default App;
+export default NumberManagement;
